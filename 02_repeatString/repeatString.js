d36e4ced6563ac0ed247c189151ab0e1cbe4c15e
pavelnaut <pavelmbogdanov@gmail.com>
2023-03-12 23:48:46 +0200

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, loops) {
+    if (loops < 0) {
+        return "ERROR"
+    }
 
+    returnString = ''
+    
+    for (; loops > 0; loops--) {
+        returnString += string
+    }
+
+    return returnString
 };
 
 // Do not edit below this line
