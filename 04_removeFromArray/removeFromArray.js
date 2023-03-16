75163b6af647e65275d7b397401b3ae7562cefa4
pavelnaut <pavelmbogdanov@gmail.com>
2023-03-16 15:54:59 +0200

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+    for (let elementToRemove of elementsToRemove) {
+        array = array.filter(element => element !== elementToRemove);
+      }
+    return array;
 };
 
 // Do not edit below this line
