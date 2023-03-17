1e33771588230371b86385a2d2e75b991788c1a5
pavelnaut <pavelmbogdanov@gmail.com>
2023-03-17 16:34:05 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(Num1, Num2) {
+    // integer check
+    if ((typeof(Num1) != 'number') || (typeof(Num2) != 'number')) {
+        return "ERROR";
+    }
 
+    // negative numbers check
+    if ((Num1 < 0) || (Num2 < 0)) {
+        return "ERROR";
+    }
+
+    // sort bigger argument
+    let fromNumber = Num1;
+    let toNumber = Num2;
+    if (Num1 > Num2) {
+        fromNumber = Num2;
+        toNumber = Num1;
+    }
+
+    // sum numbers
+    let sum = fromNumber;
+    for (let i = 1; (fromNumber + i) <= toNumber; i++) {
+        sum += (fromNumber + i);
+      }
+
+    return sum;
 };
 
 // Do not edit below this line
